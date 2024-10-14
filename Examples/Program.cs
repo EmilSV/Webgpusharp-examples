ee0a4a6e87928da6d830e27b04eef68c6c142e54
Emil Vad <emilsvad@gmail.com>
2024-10-14 17:09:04 +0200

    wip start of a simple triangle example

---
+++ Examples/Program.cs
@@ -0,0 +1,6 @@
+﻿using Nito.AsyncEx;
+//To allow async event though GLFW liked being used in a single thread 
+AsyncContext.Run(async () =>
+{
+    await HelloTriangle.Run();
+});
---
+++ Examples/SDLWebgpu.cs
@@ -0,0 +1,33 @@
+using WebGpuSharp;
+using static SDL2.SDL;
+
+static class SDLWebgpu
+{
+    public static unsafe Surface? SDL_GetWGPUSurface(Instance instance, nint window)
+    {
+        SDL_SysWMinfo windowWMInfo = new();
+        SDL_VERSION(out windowWMInfo.version);
+        SDL_GetWindowWMInfo(window, ref windowWMInfo);
+
+        if (windowWMInfo.subsystem == SDL_SYSWM_TYPE.SDL_SYSWM_WINDOWS)
+        {
+            var wsDescriptor = new WebGpuSharp.FFI.SurfaceDescriptorFromWindowsHWNDFFI()
+            {
+                Value = new WebGpuSharp.FFI.SurfaceSourceWindowsHWNDFFI()
+                {
+                    Hinstance = (void*)windowWMInfo.info.win.hinstance,
+                    Hwnd = (void*)windowWMInfo.info.win.window,
+                    Chain = new()
+                    {
+                        SType = SType.SurfaceSourceWindowsHWND
+                    }
+                }
+            };
+
+            SurfaceDescriptor descriptor_surface = new(ref wsDescriptor);
+            return instance.CreateSurface(descriptor_surface);
+        }
+
+        throw new Exception("Platform not supported");
+    }
+}
